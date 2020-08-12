eda2da2afec639f789ff72e2d2a75f12263dc7e7
Brian Farr <64746326+BriansFarr@users.noreply.github.com>
2020-08-12 19:29:29 -0400

    updating routes and controllers

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -38,6 +38,11 @@ class FoodsController < ApplicationController
     @food.destroy
   end
 
+  def add_restaurant
+    @food = Food.find(params[:id])
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_food
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :restaurants
+  resources :restaurants, only: :index
   resources :foods
   resources :users, only: :create
   post '/auth/login', to: 'authentication#login'
   get '/auth/verify', to: 'authentication#verify'
+  put '/restaurants/:restaurant_id/foods/:id'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
