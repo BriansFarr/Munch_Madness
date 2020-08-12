9640b372dae334fe1578c52fc451393ee372b507
Brian Farr <64746326+BriansFarr@users.noreply.github.com>
2020-08-12 19:07:27 -0400

    db:migrate rest, food,

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_12_223030) do
+ActiveRecord::Schema.define(version: 2020_08_12_230357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "foods", force: :cascade do |t|
+    t.string "name"
+    t.string "review"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_foods_on_user_id"
+  end
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -23,4 +38,5 @@ ActiveRecord::Schema.define(version: 2020_08_12_223030) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "foods", "users"
 end
