40da8e6726a19a976cab0e921be4da08c17b7f24
Brian Farr <64746326+BriansFarr@users.noreply.github.com>
2020-08-12 19:13:21 -0400

    foods/flavors join table migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_12_230357) do
+ActiveRecord::Schema.define(version: 2020_08_12_231046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,11 @@ ActiveRecord::Schema.define(version: 2020_08_12_230357) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_foods_on_user_id"
+  end
+
+  create_table "foods_restaurants", id: false, force: :cascade do |t|
+    t.bigint "food_id", null: false
+    t.bigint "restaurant_id", null: false
   end
 
   create_table "restaurants", force: :cascade do |t|
