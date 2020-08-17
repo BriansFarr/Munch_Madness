dcfa0505ddddb7e0d121b4e390e4830d6168a619
Brian Farr <64746326+BriansFarr@users.noreply.github.com>
2020-08-17 00:09:04 -0400

    footer fixed

--- client/src/components/ShowRestaurants.jsx
+++ client/src/components/ShowRestaurants.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
+import "../css/ShowRest.css"
 
 
 export default function ShowRestaurants(props) {
   return (
-    <div>
+    <div className="rest">
       <h3>Restaurants</h3>
       {props.restaurants.map((restaurant) => (
         <>
+          <div className="restInfo">
         <p>{restaurant.name}</p>
-          <img src={restaurant.image} width={500} height={300} mode='fit'></img>
-          
+          <img src={restaurant.image} width={250} height={175} mode='fit'></img>
+          </div>
           </>
       ))}
     </div>
